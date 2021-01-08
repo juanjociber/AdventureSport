24437968cb6a172c3156940be38c755a7cb4179c
juanjociber <72362179+juanjociber@users.noreply.github.com>
2021-01-08 13:32:58 -0500

    Add files via upload

---
+++ js/frase.js
@@ -0,0 +1,6 @@
+Swal.fire({ title:"Bienvenido a Todo Deporte Juanjo Huiza!!",
+            icon:'success',
+            html:"Nunca permitas que una buena crisis se desperdicie. Es una buena oportunidad para hacer las cosas que alguna vez pensaste eran imposibles.",
+            confirmButtonText:'Ingresar',
+            position:'center'
+});
